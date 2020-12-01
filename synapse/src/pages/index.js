fc3aa98a0be5889e7d74e37a9d22391c4a458e78
bruno deguil-robin <b.deguil-robin@netfective.com>
2020-12-02 00:30:18 +0100

    install prismic config

--- synapse/src/pages/index.js
+++ synapse/src/pages/index.js
@@ -5,10 +5,11 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
+    {console.log(data)}
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>toto</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
@@ -16,6 +17,7 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Link to='/landingPage'>landing page</Link>
   </Layout>
 )
 
